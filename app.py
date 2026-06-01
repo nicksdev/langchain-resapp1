ee9d7434142249e05a174e5805799d701d28af29
Nick Hughes <nick@nickhughes.io>
2026-06-01 12:59:01 +1000

    API as os env variable

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 import os
 
+print (os.getenv("API_KEY"))
 api_key = os.getenv("API_KEY")
 
 
 app = FastAPI()
 llm = ChatOpenAI(
     model="gpt-4o-mini",
-    api_key=os.environ["OPENAI_API_KEY"]
+    api_key=os.environ["API_KEY"]
 )
 @app.get("/")
 async def root():
